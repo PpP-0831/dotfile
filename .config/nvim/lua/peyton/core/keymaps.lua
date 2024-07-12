4e662951a6d0024d8846ff4c6ac2c427b5c2924a
PpP-0831 <losegameplayer@gmail.com>
2024-07-12 22:20:46 +0800

    Neovim config update

--- .config/nvim/lua/peyton/core/keymaps.lua
+++ .config/nvim/lua/peyton/core/keymaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
 vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
